37893200cd62fb9836a6a157f762951489c66a85
Gabrielle Lima <gabrielle.clima23@gmail.com>
2023-04-08 23:47:09 -0300

    Add new methods to controller

--- src/main/java/com/impacta/resources/TaskController.java
+++ src/main/java/com/impacta/resources/TaskController.java
@@ -1,10 +1,30 @@
 package com.impacta.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.impacta.dto.TaskDTO;
+import com.impacta.services.TaskService;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 	
+	@Autowired
+	private TaskService taskService;
+	
+	
+	@GetMapping("/tasks/{id}")
+	public ResponseEntity<TaskDTO> findById(@PathVariable Long id) {
+		TaskDTO task = taskService.findById(id);
+		return ResponseEntity.ok().body(task);
+	}
+	
+	// TODO: make update method and insert in controller
 	
 	
 
