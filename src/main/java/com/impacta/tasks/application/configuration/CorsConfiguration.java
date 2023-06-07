c0a04d3d97fd91f172bebeb52b455a506898b7ea
Gabrielle Lima <gabrielle.clima23@gmail.com>
2023-06-06 22:53:37 -0300

    feat: Add cors variable for configuration on Railway

--- src/main/java/com/impacta/tasks/application/configuration/CorsConfiguration.java
+++ src/main/java/com/impacta/tasks/application/configuration/CorsConfiguration.java
@@ -3,9 +3,13 @@ package com.impacta.tasks.application.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${cors.origins}")
+	private String corsOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
