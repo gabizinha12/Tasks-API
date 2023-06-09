3071fca0083f59e6af0162f6475550cde475dfa1
Gabrielle Lima <gabrielle.clima23@gmail.com>
2023-06-09 16:03:28 -0300

    feat: Change not empty to not blank

--- src/main/java/com/impacta/tasks/model/Task.java
+++ src/main/java/com/impacta/tasks/model/Task.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "task")
